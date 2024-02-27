a17898077d8cf34ecd9778d7b0f4ba88e9b9e777
Nathan Thomas <nathanthomas707@gmail.com>
2024-02-27 14:13:48 -0800

    Format code

--- utils.py
+++ utils.py
@@ -1,9 +1,8 @@
 import os
+import random
+
 import numpy as np
 import torch
-import random
-import re
-
 
 
 def check_directories(args):
@@ -11,16 +10,16 @@ def check_directories(args):
     if not os.path.exists(task_path):
         os.mkdir(task_path)
         print(f"Created {task_path} directory")
-    
+
     folder = args.task
-    
+
     save_path = os.path.join(task_path, folder)
     if not os.path.exists(save_path):
         os.mkdir(save_path)
         print(f"Created {save_path} directory")
     args.save_dir = save_path
 
-    cache_path = os.path.join(args.input_dir, 'cache')
+    cache_path = os.path.join(args.input_dir, "cache")
     if not os.path.exists(cache_path):
         os.mkdir(cache_path)
         print(f"Created {cache_path} directory")
@@ -30,6 +29,7 @@ def check_directories(args):
 
     return args
 
+
 def set_seed(args):
     random.seed(args.seed)
     np.random.seed(args.seed)
@@ -37,12 +37,13 @@ def set_seed(args):
     if args.n_gpu > 0:
         torch.cuda.manual_seed_all(args.seed)
 
+
 def setup_gpus(args):
     n_gpu = 0  # set the default to 0
     if torch.cuda.is_available():
         n_gpu = torch.cuda.device_count()
     args.n_gpu = n_gpu
-    if n_gpu > 0:   # this is not an 'else' statement and cannot be combined
+    if n_gpu > 0:  # this is not an 'else' statement and cannot be combined
         torch.backends.cudnn.benchmark = False
         torch.backends.cudnn.deterministic = True
     return args
